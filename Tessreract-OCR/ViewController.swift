b8963a24f9f36ecdd1ed505acda7cd0415c75b51
sz ashik <szashik@Sayeds-MBP.lan>
2018-02-07 18:57:45 +0600

    Add tessract tessdata file project

--- Tessreract-OCR/ViewController.swift
+++ Tessreract-OCR/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import TesseractOCR
 
 class ViewController: UIViewController {
+    
+    let imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func uploadAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
+}
 
-
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func openCamera() {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallary() {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            if let tesseract = G8Tesseract(language: "eng") {
+                print("Trying to convert image to text")
+                tesseract.engineMode = .tesseractCubeCombined
+                tesseract.pageSegmentationMode = .auto
+                
+                tesseract.image = editedImage.g8_blackAndWhite()
+                
+                tesseract.recognize()
+                print(tesseract.recognizedText)
+                
+                
+//                textView.text = tesseract.recognizedText
+            } else {
+                print("Tesseract not working")
+            }
+//            profileImage.image = editedImage
+//            profileImage.rounded = true
+//            uploadAvater(image: editedImage)
+//            profileImage.clipsToBounds = true
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
